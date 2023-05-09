3970a504185bdf9346567ae457ebfc085a4442da
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-05-09 11:08:51 +0200

    Add export to environment file

--- ginkgo/99-ginkgo-env.sh
+++ ginkgo/99-ginkgo-env.sh
@@ -1,4 +1,6 @@
 # Copy this file into /etc/profile.d/
 
+export Ginkgo_DIR=/ginkgo/build
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Ginkgo_DIR/lib
 export CPATH=$CPATH:$Ginkgo_DIR/include
--- ginkgo/ginkgo-install.sh
+++ ginkgo/ginkgo-install.sh
@@ -1,8 +1,6 @@
 # This script installs GINKGO
 VERSION=3.12.4
 
-export Ginkgo_DIR=$(pwd)/ginkgo/build
-
 wget -c https://github.com/ginkgo-project/ginkgo/archive/refs/heads/public_common_kernels.zip
 unzip public_common_kernels.zip && mv ginkgo-public_common_kernels ginkgo
 cd ginkgo && mkdir build && cd build
