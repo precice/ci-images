b092d8914dd1d096a0f3c9fb85935f40e1d84047
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-07-16 19:04:31 +0200

    Disable build testing

--- ginkgo/ginkgo-install.sh
+++ ginkgo/ginkgo-install.sh
@@ -4,4 +4,4 @@ VERSION=1.8.0
 wget -c https://github.com/ginkgo-project/ginkgo/archive/refs/tags/v$VERSION.zip
 unzip v$VERSION.zip && mv ginkgo-$VERSION ginkgo
 cd ginkgo && mkdir build && cd build
-cmake .. && make
+cmake -DBUILD_TESTING="OFF" .. && make
