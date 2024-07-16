b913081e59df8480bbc0db00eecf6a9dcaf6f850
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-07-16 19:06:53 +0200

    Reduce compile time of ginkgo further

--- ginkgo/ginkgo-install.sh
+++ ginkgo/ginkgo-install.sh
@@ -4,4 +4,4 @@ VERSION=1.8.0
 wget -c https://github.com/ginkgo-project/ginkgo/archive/refs/tags/v$VERSION.zip
 unzip v$VERSION.zip && mv ginkgo-$VERSION ginkgo
 cd ginkgo && mkdir build && cd build
-cmake -DBUILD_TESTING="OFF" .. && make
+cmake -DBUILD_TESTING="OFF" -DGINKGO_BUILD_EXAMPLES="OFF" -DGINKGO_BUILD_TESTS="OFF" .. && make
