447f3078978fe8ef68de31f1e6917daebd4f7bb0
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2026-01-07 16:41:31 +0100

    Add kokkos-kernels to image

---
+++ kokkos-kernels/99-kokkos-kernels-env.sh
@@ -0,0 +1,6 @@
+# Copy this file into /etc/profile.d/
+
+export KokkosKernels_DIR=/kokkos-kernels/build
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KokkosKernels_DIR/lib
+export CPATH=$CPATH:$KokkosKernels_DIR/include
---
+++ kokkos-kernels/kokkos-kernels-install.sh
@@ -0,0 +1,7 @@
+# This script installs Kokkos-Kernels
+VERSION=4.6.02
+
+wget -c https://github.com/kokkos/kokkos-kernels/archive/refs/tags/$VERSION.zip
+unzip $VERSION.zip && mv kokkos-kernels-$VERSION kokkos-kernels
+cd kokkos-kernels && mkdir build && cd build
+cmake -DBUILD_TESTING="OFF" -DBUILD_SHARED_LIBS="ON" .. && make -j 4
